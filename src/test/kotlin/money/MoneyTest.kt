e4a7127d42f799a0db3116fe4ddff2c89d78a882
kenshi1202 <kenshi1202@gmail.com>
2021-12-11 23:56:49 +0900

    chapter 9 in progress

--- src/test/kotlin/money/MoneyTest.kt
+++ src/test/kotlin/money/MoneyTest.kt
@@ -17,9 +17,9 @@ class MoneyTest {
     fun testEquality() {
         assertTrue(Money.dollar(5).equals(Money.dollar(5)));
         assertFalse(Money.dollar(5).equals(Money.dollar(6)));
-        assertTrue(Franc(5).equals(Franc(5)));
-        assertFalse(Franc(5).equals(Franc(6)));
-        assertFalse(Franc(5).equals(Money.dollar(5)));
+        assertTrue(Money.franc(5).equals(Money.franc(5)));
+        assertFalse(Money.franc(5).equals(Money.franc(6)));
+        assertFalse(Money.franc(5).equals(Money.dollar(5)));
     }
 
     @Test
@@ -27,6 +27,12 @@ class MoneyTest {
         var five = Money.franc(5) as Money
         assertEquals(Money.franc(10), five.times(2))
         assertEquals(Money.franc(15), five.times(3))
+    }
+
+    @Test
+    fun testCurreny() {
+        assertEquals("USD", Money.dollar(1).currency())
+        assertEquals("CHF", Money.franc(1).currency())
     }
 
 }
